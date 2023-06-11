4efdea9d49cf991766338e5a3d1c51316f0b5277
Marko Milev <65156756+milev051@users.noreply.github.com>
2023-06-11 16:05:37 +0200

    Start

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function handleImageClick(imageElement) {
+    // Get the source and alt text of the clicked image
+    var imageUrl = imageElement.getAttribute("src");
+    var imageAlt = imageElement.getAttribute("alt");
+
+    // Create the popup content
+    var popupContent = document.createElement("div");
+    popupContent.className = "popup-content";
+    popupContent.innerHTML = `
+        <img src="${imageUrl}" alt="${imageAlt}" class="popup-image">
+        <h3>${imageAlt}</h3>
+    `;
+
+    // Create the popup container
+    var popupContainer = document.createElement("div");
+    popupContainer.className = "popup-container";
+    popupContainer.appendChild(popupContent);
+
+    // Append the popup container to the body
+    document.body.appendChild(popupContainer);
+
+    // Add event listener to close the popup when clicked outside the content
+    popupContainer.addEventListener("click", function (event) {
+        if (!popupContent.contains(event.target)) {
+            document.body.removeChild(popupContainer);
+        }
+    });
+}
+
+function handlePopupImages() {
+    var popupImages = document.querySelectorAll(".popup-image");
+
+    popupImages.forEach(function (image) {
+        image.addEventListener("click", function () {
+            handleImageClick(image);
+        });
+    });
+}
+
+// Call the function to handle the popup images
+handlePopupImages();
