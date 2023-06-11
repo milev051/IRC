debc37c5e6f5b0092029750bd1ee147e9a659f38
Marko Milev <milev051@gmail.com>
2023-06-11 18:10:42 +0200

    JS Popup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 function showPopup() {
   var popup = document.getElementById("popup");
   popup.style.display = "flex";
+
+  // var popupElement = document.querySelector(".popup");
+  // popupElement.display = "flex";
+
+  // var elements = document.getElementsByClassName("popup");
+  // var popupElement = elements[0]; // Assuming you want the first matching element
+  // popupElement.display = "flex";
 }
 
 function hidePopup() {
