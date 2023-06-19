2f505fdeaa12686cc789629c32cba7ad44155179
Marko Milev <milev051@gmail.com>
2023-06-19 05:12:23 +0200

    20230619_051221

---
+++ script.js
@@ -0,0 +1,47 @@
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "flex";
+
+  // var popupElement = document.querySelector(".popup");
+  // popupElement.display = "flex";
+
+  // var elements = document.getElementsByClassName("popup");
+  // var popupElement = elements[0]; // Assuming you want the first matching element
+  // popupElement.display = "flex";
+}
+
+function hidePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+// HAMBURGER MENU
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+var menu = document.getElementById("menu2");
+var logo = document.getElementById("logo");
+var hamburger = document.getElementById("hamburger");
+var header = document.getElementsByTagName("header")[0];
+function toggleMenu() {
+  if (menu.style.display === "flex") {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+}
+
+function closeMenu() {
+  menu.style.display = "none";
+  logo.src = "img/icon-irc-white.png";
+  hamburger.src = "img/icon-hamburger.png"
+  header.style.backgroundColor = "transparent";
+}
+function openMenu() {
+  menu.style.display = "flex";
+  logo.src = "img/icon-irc.png";
+  hamburger.src = "img/icon-close-button.png"
+  header.style.backgroundColor = "white";
+}
+
+window.addEventListener('resize', closeHamburgerMenu);
